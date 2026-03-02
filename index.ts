918af878dc0d7147ee9b1119b5a33594166cb7d9
lostmart <martinpedraza1979@gmail.com>
2026-03-02 21:42:33 +0100

    new installs

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from "uuid"
 
 type ProductName = "Shoes" | "Shirt" | "Pants"
 
-type PriceNumber = number & { readonly __brand: "PriceNumber" }
+type PriceNumber = number & { readonly __brand: unique symbol }
 
 function createPrice(value: number): PriceNumber {
 	if (value < 0) {
@@ -31,22 +31,19 @@ function createProduct(
 	}
 
 	return {
-		id: uuidv4(),
+		id: uuidv4() as ProductId,
 		name,
 		price,
 	}
 }
-
 type Product = {
-	id: string
+	id: ProductId
 	name: ProductName
-	price: number
+	price: PriceNumber
 }
 
 type ProductId = string & { readonly __brand: unique symbol }
 type StockLevel = number & { readonly __brand: unique symbol }
-
-type EventName = DomainEvent["type"]
 
 type ProductCreatedEvent = {
 	readonly type: "ProductCreated"
@@ -71,31 +68,28 @@ type StockReducedEvent = {
 }
 type DomainEvent = ProductCreatedEvent | PriceUpdatedEvent | StockReducedEvent
 
+type Quantity = number & { readonly __brand: unique symbol }
+
 type Observer = (event: DomainEvent) => void
 
-type Quantity = number & { readonly __brand: unique symbol }
+const observers: Observer[] = []
 
 // first product (easy construction)
 const product1: Product = {
-	id: uuidv4(),
+	id: uuidv4() as ProductId,
 	name: "Shoes",
-	price: 100,
+	price: createPrice(100),
 }
 
-const sendEmailMock = (event: DomainEvent, email: string, subject: string) => {
-	console.log(
-		`Email sent to ${email} with subject "${subject}" for event ${event.type}`,
-	)
+console.log(product1)
+
+const sendEmailMock: Observer = (event: DomainEvent) => {
+	console.log(`Email sent for event ${event.type}`)
 }
 
-const saveToDatabaseMock = (event: DomainEvent, data: any) => {
-	console.log(
-		`Data saved to database: ${JSON.stringify(data)} for event ${event.type}`,
-	)
+const saveToDatabaseMock: Observer = (event: DomainEvent) => {
+	console.log(`Data saved to database for event ${event.type}`)
 }
-
-const observers = []
-
 observers.push(sendEmailMock)
 observers.push(saveToDatabaseMock)
 
@@ -103,16 +97,12 @@ try {
 	const product2 = createProduct(uuidv4(), "Shirt", createPrice(50))
 	console.log(product2)
 	observers.forEach((observer) =>
-		observer(
-			{
-				type: "ProductCreated",
-				productId: product2.id as ProductId,
-				name: product2.name,
-				price: product2.price as PriceNumber,
-			},
-			"test one",
-			"test two",
-		),
+		observer({
+			type: "ProductCreated",
+			productId: product2.id,
+			name: product2.name,
+			price: product2.price,
+		}),
 	)
 } catch (error) {
 	if (error instanceof Error) {
